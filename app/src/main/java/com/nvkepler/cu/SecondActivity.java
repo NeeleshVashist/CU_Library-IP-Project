0c877e7cedb2ae2292d75de834832abfd0f1af8b
nvkepler <neeleshvashist@gmail.com>
2018-03-22 20:30:21 +0530

    Now can move between activites

--- app/src/main/java/com/nvkepler/cu/SecondActivity.java
+++ app/src/main/java/com/nvkepler/cu/SecondActivity.java
@@ -1,6 +1,9 @@
 package com.nvkepler.cu;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 /**
  * Created by neha on 22/3/18.
@@ -12,5 +15,60 @@ public class SecondActivity extends MainActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.acitivity_second);
 
+        //*******************************************************************************************
+
+        // Find the View that shows the IR category
+        TextView ir = (TextView) findViewById(R.id.ir);
+
+        // Set a click listener on that View
+        ir.setOnClickListener(new View.OnClickListener() {
+            // The code in this method will be executed when the IR category is clicked on.
+            @Override
+            public void onClick(View view) {
+                // Create a new intent to open the {@link IRActivity}
+                Intent IRIntent = new Intent(SecondActivity.this, IRActivity.class);
+
+                // Start the new activity
+                startActivity(IRIntent);
+            }
+        });
+
+        //*******************************************************************************************
+
+        // Find the View that shows the Books category
+        TextView book = (TextView) findViewById(R.id.books);
+
+        // Set a click listener on that View
+        book.setOnClickListener(new View.OnClickListener() {
+            // The code in this method will be executed when the IR category is clicked on.
+            @Override
+            public void onClick(View view) {
+                // Create a new intent to open the {@link BooksActivity}
+                Intent BooksIntent = new Intent(SecondActivity.this, BooksActivity.class);
+
+                // Start the new activity
+                startActivity(BooksIntent);
+            }
+        });
+
+        //*******************************************************************************************
+
+        // Find the View that shows the Student Info category
+        TextView stu = (TextView) findViewById(R.id.student);
+
+        // Set a click listener on that View
+        stu.setOnClickListener(new View.OnClickListener() {
+            // The code in this method will be executed when the IR category is clicked on.
+            @Override
+            public void onClick(View view) {
+                // Create a new intent to open the {@link StudentActivity}
+                Intent StuIntent = new Intent(SecondActivity.this, StudentActivity.class);
+
+                // Start the new activity
+                startActivity(StuIntent);
+            }
+        });
+
+        //*******************************************************************************************
     }
 }
