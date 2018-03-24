df94aabc3c990a4af0ee75c0f13a623796a2a909
nvkepler <neeleshvashist@gmail.com>
2018-03-25 02:57:58 +0530

    Mod : New avatar for Splash Screen

--- app/src/main/java/com/nvkepler/cu/MainActivity.java
+++ app/src/main/java/com/nvkepler/cu/MainActivity.java
@@ -8,7 +8,7 @@ import android.util.Log;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-    private static int SPLASH_TIME_OUT = 5000;
+    private static int SPLASH_TIME_OUT = 1000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
