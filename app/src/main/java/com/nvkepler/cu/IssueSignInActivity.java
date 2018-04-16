b31f661b5076ffac8df21d2b20ed4ad836e41eca
RajpootNeha <rajpoot0211neha@gmail.com>
2018-04-16 09:23:48 +0530

    Add : Navigation Panel

--- app/src/main/java/com/nvkepler/cu/IssueSignInActivity.java
+++ app/src/main/java/com/nvkepler/cu/IssueSignInActivity.java
@@ -1,13 +1,39 @@
 package com.nvkepler.cu;
 
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.view.MenuItem;
 
 //Created by neelv on 22/3/18.
 
+
 public class IssueSignInActivity extends FragmentsSignInActivity {
+
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mToggle;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.issue_activity);
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawerlayout);
+        mToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.open, R.string.close);
+
+        mDrawerLayout.addDrawerListener(mToggle);
+        mToggle.syncState();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (mToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
