a8b00039b1d0f5383a5d85ccc689dc561ad04a92
nvkepler <neeleshvashist@gmail.com>
2018-04-21 19:14:17 +0530

    Add : FireBase Auth

--- app/src/main/java/com/nvkepler/cu/SignInActivity.java
+++ app/src/main/java/com/nvkepler/cu/SignInActivity.java
@@ -4,23 +4,99 @@ package com.nvkepler.cu;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SignInActivity extends AppCompatActivity {
+
+    private EditText mEmailField;
+    private EditText mPasswordField;
+
+    private Button mLoginBtn;
+
+    private FirebaseAuth mAuth;
+
+    private FirebaseAuth.AuthStateListener mAuthListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.signin_activity);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        mEmailField = (EditText) findViewById(R.id.emailField);
+        mPasswordField = (EditText) findViewById(R.id.passwordField);
+
+        mLoginBtn = (Button) findViewById(R.id.loginBtn);
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+
+                if (firebaseAuth.getCurrentUser() != null) {
+
+                    startActivity(new Intent(SignInActivity.this, FragmentActivity.class));
+
+                }
+
+            }
+        };
+
+
+        mLoginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                startSignIn();
+
+            }
+        });
     }
 
-    //Connecting FragmentsActivity with SignInActivity
-    public void SignIn(View v) {
-        if (v.getId() == R.id.signin) {
-            Intent intent = new Intent(this, FragmentsActivity.class);
-            startActivity(intent);
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    private void startSignIn() {
+
+        String email = mEmailField.getText().toString();
+        String password = mPasswordField.getText().toString();
+
+        if (TextUtils.isEmpty(email) || TextUtils.isEmpty(password)) {
+
+            Toast.makeText(SignInActivity.this, "Fields are Empty", Toast.LENGTH_LONG).show();
+
+
+        } else {
+
+            mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+
+                    if (!task.isSuccessful()) {
+
+                        Toast.makeText(SignInActivity.this, "SignIn Problem", Toast.LENGTH_LONG).show();
+                    }
+
+                }
+            });
         }
-    }
 
+
+    }
 }
